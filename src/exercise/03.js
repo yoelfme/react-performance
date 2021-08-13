069211be289da5f98fbdcc7d101b6d863ccc42d7
Yoel Monzon <yoelfme@gmail.com>
2021-08-12 23:07:59 -0500

    [update] add react.memo to prevent unnecessary re-renders and passing a comparator function

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -30,7 +30,8 @@ function Menu({
     </ul>
   )
 }
-// 🐨 Memoize the Menu here using React.memo
+
+Menu = React.memo(Menu)
 
 function ListItem({
   getItemProps,
@@ -56,7 +57,22 @@ function ListItem({
     />
   )
 }
-// 🐨 Memoize the ListItem here using React.memo
+
+ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+  if (prevProps.getItemProps !== nextProps.getItemProps) return false
+  if (prevProps.index !== nextProps.index) return false
+  if (prevProps.item !== nextProps.item) return false
+  if (prevProps.selectedItem !== nextProps.selectedItem) return false
+
+  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
+    const wasHighlighted = prevProps.highlightedIndex === prevProps.index
+    const isHighlighted = nextProps.highlightedIndex === nextProps.index
+
+    return wasHighlighted === isHighlighted
+  }
+
+  return true
+})
 
 function App() {
   const forceRerender = useForceRerender()
